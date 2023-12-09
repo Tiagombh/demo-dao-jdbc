a9ef9c5c53db0eaba1fb02f3d28e92fe7475f2ba
Tiago Magalhães <tiagomagalhaes.bh@gmail.com>
2023-12-09 17:27:01 +0000

    insert implementation in department

---
+++ src/application/Program2.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		System.out.println();
+		
+		
+		System.out.println("\n######Test 1: department insert#####");
+		Department newDepartment = new Department(5, "Music");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New department "+newDepartment.getId());
+	}
+
+}
+
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,16 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDao_JDBC;
 import model.dao.impl.SellerDao_JDBC;
 
 public class DaoFactory {
 	public static SellerDao createSellerDao() {
 		return new SellerDao_JDBC (DB.getConnection());
-
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDao_JDBC(DB.getConnection()); 
 	}
 
 }
